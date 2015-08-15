892d360a9a21ad98bb3282fd4b8319096f0b91fb
Dongtao Yu <dongtaoy@Dongtaos-MacBook-Pro-2.local>
2015-08-15 21:16:32 +1000

    test menuitemlabel class

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -38,15 +38,63 @@ bool MainMenuScene::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
-    LabelTTF *startMultiplayerLabel = LabelTTF::create("Multiplayer", "Marker Felt", 30);
-    startMultiplayerLabel->setColor(Color3B::BLACK);
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
     
-//    MenuItemLabel *startMultiplayerButton = MenuItemLabel::create(startMultiplayerLabel, CC_CALLBACK_3(MainMenuScene::,
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(MainMenuScene::menuCloseCallback, this));
+    
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+    
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+    
+    /////////////////////////////
+    // 3. add your codes below...
+    
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    label->setColor(Color3B::WHITE);
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+    
+    
+    auto helloworldButton = MenuItemLabel::create(label, CC_CALLBACK_1(MainMenuScene::menuCloseCallback, this));
+    auto menu1 = Menu::create(helloworldButton, NULL);
+    menu1->setPosition(Point::ZERO);
+    this->addChild(menu1, 1);
+    
+//    // add "HelloWorld" splash screen"
+//    auto sprite = Sprite::create("HelloWorld.png");
 //    
+//    // position the sprite on the center of the screen
+//    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 //    
-
+//    // add the sprite as a child to this layer
+//    this->addChild(sprite, 0);
+    
+    
     
     
     return true;
 }
+
+void MainMenuScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+
 
