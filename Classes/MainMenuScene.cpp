75dae32fee32b0f60bba0eff375158d33681aefa
Dongtao Yu <dongtaoy@Dongtaos-MacBook-Pro-2.local>
2015-08-15 19:51:32 +1000

    add definations and splash scene and mainmenuscene

---
+++ Classes/MainMenuScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MainMenuScene.cpp
+//  KOF
+//
+//  Created by Dongtao Yu on 12/08/2015.
+//
+//
+
+#include "MainMenuScene.h"
---
+++ Classes/MainMenuScene.h
@@ -0,0 +1,14 @@
+//
+//  MainMenuScene.h
+//  KOF
+//
+//  Created by Dongtao Yu on 12/08/2015.
+//
+//
+
+#ifndef __KOF__MainMenuScene__
+#define __KOF__MainMenuScene__
+
+#include <stdio.h>
+
+#endif /* defined(__KOF__MainMenuScene__) */
