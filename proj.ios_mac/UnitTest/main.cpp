65305b8c5f1d2b0322b01d0cef34b5fc2e348123
Derek <deruiw@student.unimelb.edu.au>
2015-11-01 15:43:44 +1100

    unit test

--- proj.ios_mac/UnitTest/main.cpp
+++ proj.ios_mac/UnitTest/main.cpp
@@ -1,9 +1,9 @@
 //
 //  main.cpp
-//  TestRunner
+//  UnitTest
 //
-//  Created by Hilton Lipschitz on 2014-10-09.
-//  Copyright (c) 2014 Hilton Lipschitz. All rights reserved.
+//  Created by 小七爷 on 15/11/1.
+//
 //
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
@@ -11,12 +11,12 @@
 
 
 /* We were planing to do unit test by using framework Catch.But we find it is very hard to implement
-We fix most problems that we have come across. But when we try to link with Photon, it gives mactype error
-It initially takes us about 1 week time to fix this error alst time. We don't really have time to solve it this time.
-Also none of team members know how to program in Objective-c or swift. That is why we cannnot use XTest built in Xcode.
-Instead we choose to do written unit test and add assert() into key part of our code to do unit test.
-We also do usability test， related interview and analyze report.
+ We fix most problems that we have come across. But when we try to link with Photon, it gives mactype error
+ It initially takes us about 1 week time to fix this error alst time. We don't really have time to solve it this time.
+ Also none of team members know how to program in Objective-c or swift. That is why we cannnot use XTest built in Xcode.
+ Instead we choose to do written unit test and add assert() into key part of our code to do unit test.
+ We also do usability test， related interview and analyze report.
  
-We should evalute the risk that this could happen before we do this. 
-Also we should do unit test right after when we finish one key component. This is a good reminder for us for our future work.
-*/
+ We should evalute the risk that this could happen before we do this.
+ Also we should do unit test right after when we finish one key component. This is a good reminder for us for our future work.
+ */
